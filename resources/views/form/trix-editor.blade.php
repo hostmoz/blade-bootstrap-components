d8cac89cab554f01f12c2288d7145671792e4640
Elisio Leonardo <elisio.leonardo@gmail.com>
2024-09-16 22:49:30 +0200

    Trix Editor

--- resources/views/form/trix-editor.blade.php
+++ resources/views/form/trix-editor.blade.php
@@ -3,3 +3,12 @@
     <input id="{{ $name }}" type="hidden" name="{{ $name }}" value="{{$value}}">
     <trix-editor input="{{ $name }}"></trix-editor>
 </div>
+@once
+    @push('styles')
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
+    @endpush
+
+    @push('scripts')
+        <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
+    @endpush
+@endonce
