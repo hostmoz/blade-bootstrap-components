ec7d3220702544412f200578e3dfeb894f7bbc8d
Elisio Leonardo <elisio.leonardo@gmail.com>
2024-09-17 06:04:37 +0200

    First deployment

--- resources/views/form/trix-editor.blade.php
+++ resources/views/form/trix-editor.blade.php
@@ -1,7 +1,7 @@
 <div class=" mb-3">
     <x-bootstrap::form.label :label="$label" :for="$name" class="{{$required?'required':''}}"/>
     <input id="{{ $name }}" type="hidden" name="{{ $name }}" value="{{$value}}">
-    <trix-editor input="{{ $name }}"></trix-editor>
+    <trix-editor input="{{ $name }}" class="trix-content"></trix-editor>
 </div>
 @once
     @push('styles')
