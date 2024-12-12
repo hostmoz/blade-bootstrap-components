4332538e6b9ad8884d7eca525bcd054bc17faa45
Elisio Leonardo <elisio.leonardo@gmail.com>
2024-12-12 04:12:22 +0200

    Client Module Initialization

--- resources/views/livewire/dependent-selects.blade.php
+++ resources/views/livewire/dependent-selects.blade.php
@@ -1,8 +1,8 @@
 <div class="row p-2">
     <!-- Seleção de País -->
-    <div class="form-group col-4">
+    <div class="form-group col-sm-4">
         <label for="{{ $countryField }}">{{$countryLabel}}</label>
-        <select id="{{ $countryField }}" class="form-control" wire:model.live="selectedCountry"
+        <select id="{{ $countryField }}" class="form-select" wire:model.live="selectedCountry"
                 name="{{$countryField}}">
             <option value="">Selecione um país</option>
             @foreach($countries as $id => $name)
@@ -13,9 +13,9 @@
     </div>
 
     <!-- Seleção de Província -->
-    <div class="form-group col-4">
+    <div class="form-group col-sm-4">
         <label for="{{ $provinceField }}">{{$provinceLabel}}</label>
-        <select id="{{ $provinceField }}" class="form-control" name="{{$provinceField}}"
+        <select id="{{ $provinceField }}" class="form-select" name="{{$provinceField}}"
                 wire:model.live="selectedProvince" {{ empty($provinces) ? 'disabled' : '' }}>
             <option value="">Selecione uma província</option>
             @foreach($provinces as $id => $name)
@@ -26,9 +26,9 @@
     </div>
 
     <!-- Seleção de Distrito -->
-    <div class="form-group col-4">
+    <div class="form-group col-sm-4">
         <label for="{{ $districtField }}">{{$districtLabel}}</label>
-        <select id="{{ $districtField }}" class="form-control" name="{{$districtField}}"
+        <select id="{{ $districtField }}" class="form-select" name="{{$districtField}}"
                 wire:model.live="selectedDistrict" {{ empty($districts) ? 'disabled' : '' }}>
             <option value="">Selecione um distrito</option>
             @foreach($districts as $id => $name)
